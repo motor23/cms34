b151e7d74544d1a3a532d77f9a94b4915f3e39c9
Andrey Talantsev <slivtime@gmail.com>
2015-11-20 18:25:30 +0300

    Fix recursive delete for sections

--- cms34/stream/stream_handlers.py
+++ cms34/stream/stream_handlers.py
@@ -14,6 +14,27 @@ class RecursiveDeleteFlagItemHandler(DeleteItemHandler):
             env.db.add(log)
         self.clear_tray(env, item)
 
+    def _get_edit_url(self, env, item):
+        for stream in env.streams.values():
+            url = stream.get_edit_url(env, item)
+            if url is not None:
+                return url
+
+    def _list_referrers(self, env, item):
+        """
+        Base version of this method checks all properties of all models that
+        points to this item. It is very slow, because it also checks inherited
+        relations. This version returns only children for given item and do not
+        try to find relations of other models.
+        """
+        sections = env.db.query(env.models.Section).filter_by(parent_id=item.id)
+        refs = {}
+        for section in sections:
+            url = self._get_edit_url(env, section)
+            if url is not None:
+                refs[section] = url
+        return refs
+
     def delete_item_handler(self, env, data):
         insure_is_xhr(env)
         item, edit_session, lock_message, filter_form = \
@@ -28,16 +49,16 @@ class RecursiveDeleteFlagItemHandler(DeleteItemHandler):
         delete_url = stream.url_for(env, 'delete', item=item.id) \
             .qs_set(filter_form.get_data())
 
-        all_referers = self._list_referers(env, item)
-        section_referers = {ref: url for (ref, url) in all_referers.items() if
-                            getattr(ref, 'parent_id', None) == item.id}
-        other_referers = {ref: url for (ref, url) in all_referers.items() if
-                          ref not in section_referers}
+        all_referrers = self._list_referrers(env, item)
+        section_referrers = {ref: url for (ref, url) in all_referrers.items() if
+                             getattr(ref, 'parent_id', None) == item.id}
+        other_referrers = {ref: url for (ref, url) in all_referrers.items() if
+                           ref not in section_referrers}
 
         if env.request.method == 'POST':
             self.stream.insure_has_permission(env, 'd')
 
-            for section in section_referers.keys():
+            for section in section_referrers.keys():
                 self.mark_as_deleted(env, section)
             self.mark_as_deleted(env, item)
 
@@ -49,8 +70,8 @@ class RecursiveDeleteFlagItemHandler(DeleteItemHandler):
         data = dict(item=item,
                     item_url=item_url,
                     form_url=delete_url,
-                    section_referers=section_referers,
-                    other_referers=other_referers,
+                    section_referers=section_referrers,
+                    other_referers=other_referrers,
                     title=u'Удаление объекта «%s»' % item,
                     stream=stream,
                     stream_url=stream_url,
