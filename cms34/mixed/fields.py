7ba5f56e815dc2265b905e1ee8c2422778c40075
Andrey Talantsev <slivtime@gmail.com>
2016-09-13 15:02:48 +0300

    Fix slug uniqueness validator

--- cms34/mixed/fields.py
+++ cms34/mixed/fields.py
@@ -237,9 +237,10 @@ def slug_uniqueness(converter, value):
     Slug should be unique on current section tree level.
     """
     current_item = converter.field.form.item
+    form_parent_id = converter.field.form.raw_data.get('parent')
     env = converter.field.form.env
     slug_exists = env.db.query(env.models.Section) \
-        .filter_by(parent_id=current_item.parent_id, slug=value) \
+        .filter_by(parent_id=form_parent_id, slug=value) \
         .filter(env.models.Section.id != current_item.id,
                 env.models.Section.state != env.models.Section.DELETED).count()
 
