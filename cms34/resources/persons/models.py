12f3f618ad6fa50a6ddc69d09370c7228b53bcfe
Andrey Talantsev <slivtime@gmail.com>
2016-09-13 15:01:47 +0300

    Add related events for orgs and persons

--- cms34/resources/persons/models.py
+++ cms34/resources/persons/models.py
@@ -16,6 +16,7 @@ from .fields import (
     xf_post,
 )
 
+
 class MFY_Person(ModelFactory):
     model = 'Person'
     title = u'Персона'
@@ -30,19 +31,45 @@ class MFY_Person(ModelFactory):
         MF_Img('img_orig'),
     ]
 
-   # @hybrid_factory_method.factory
-   # @cached_property
-   # def title(self):
-   #     return u'Персона'
-   # 
-   # @title.model
-   # @property
-   # def title(self):
-   #     return u' '.join([self.last_name, self.first_name, self.patronymic])
+    # @hybrid_factory_method.factory
+    # @cached_property
+    # def title(self):
+    #     return u'Персона'
+    # 
+    # @title.model
+    # @property
+    # def title(self):
+    #     return u' '.join([self.last_name, self.first_name, self.patronymic])
+
+    @hybrid_factory_method.model
+    def related_events(self, env):
+        """
+        Query all EventsListSections for any children events related to
+        this object.
+        :return: [(section, [events]), ...]
+        """
+        result = []
+        Event = getattr(self.models, 'Event', None)
+        EventsListSection = getattr(self.models, 'EventsListSection', None)
+        Event_Section = getattr(self.models, 'Event_Section', None)
+        if not all([Event, Event_Section, EventsListSection]):
+            return result
+
+        event_list_sections = env.db.query(EventsListSection)
+        for section in event_list_sections:
+            events = env.db.query(Event) \
+                .filter(Event.section_id == section.id) \
+                .join(Event_Section) \
+                .filter(Event_Section.section_id == self.id) \
+                .order_by(Event.dt.desc()) \
+                .limit(3) \
+                .all()
+            if events:
+                result.append((section, events))
+        return result
 
 
 class PersonsListSection(ModelFactory):
     title = u'Список персон'
 
     fields = [xf_id]
-
